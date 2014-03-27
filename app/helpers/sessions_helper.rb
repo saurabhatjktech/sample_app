6860341380336b1782116b6401debdcb3f255c6f
Saurabh Pandey <saurabhkumar.pandey@jktech.com>
2014-03-27 18:45:07 +0530

    finish the user deletion

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,6 +9,10 @@ module SessionsHelper
   def current_user=(user)
     @current_user = user
   end
+
+  def current_user?(user)
+    user == current_user
+  end
   def current_user
     remember_token = User.hash(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
@@ -22,5 +26,14 @@ module SessionsHelper
                                   User.hash(User.new_remember_token))
     cookies.delete(:remember_token)
     self.current_user = nil
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
   end
 end
